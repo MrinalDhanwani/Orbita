01d4c77c31ea1136413f8da6b71314f391068738
MirnalDhanwani <mirnaldhanwani@gmail.com>
2026-06-16 17:07:53 +0530

    connected MongoDB Atlas database to backend

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,11 @@
 require('dotenv').config();
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log('MongoDB connected successfully 🪐'))
+  .catch((err) => console.log('MongoDB connection error:', err));
+
+
 const express = require('express');
 const cors = require('cors');
 const Groq = require('groq-sdk');
